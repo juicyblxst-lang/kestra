7955712c5832bdc574ebbc9b7da9c19e6488fdef
Breeze <juicyblxst@gmail.com>
2026-09-10 17:29:56 -0700

    feat(web): add issuer wallet hook

---
+++ card-settlement-rail/apps/web-issuer/src/hooks/use-wallet.ts
@@ -0,0 +1,3 @@
+"use client";
+import { useAccount,useConnect,useDisconnect } from "wagmi";
+export function useWallet(){const account=useAccount();const {connectors,connect,isPending}=useConnect();const {disconnect}=useDisconnect();return {...account,connectors,isPending,connect:(connector=connectors[0])=>connector&&connect({connector}),disconnect}}
